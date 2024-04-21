07c368cc1be6d7b353ccf5bd55e62954533b2522
Nabethse <152297484+Nabethse@users.noreply.github.com>
2024-04-21 17:08:06 -0600

    Delete BufetAdogados directory

--- BufetAdogados/src/main/java/com/nabethse/bufetadogados/models/GestorUsuarios.java
+++
@@ -1,8 +0,0 @@
-package com.nabethse.bufetadogados.models;
-
-import java.util.ArrayList;
-
-public interface GestorUsuarios
-{boolean save (Usuarios usuarios);
-    ArrayList<Usuarios> getAllUsuarios();
-}
